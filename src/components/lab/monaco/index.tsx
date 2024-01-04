1975c456b8139e875533525a9286b5e915cfcbec
malsuke <sukemal096@gmail.com>
2024-01-04 20:15:09 +0900

    update: デモ対応

--- src/components/lab/monaco/index.tsx
+++ src/components/lab/monaco/index.tsx
@@ -51,6 +51,58 @@ interface File {
   value: string
 }
 
+type EditorConfig = {
+  mode: string
+  code: string
+}
+
+export const data: EditorConfig = {
+  mode: 'php',
+  code: `<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <title>ログインフォーム</title>
+  </head>
+  <body>
+  <?php 
+  
+  $dsn = 'mysql:dbname=db;host=db';
+  $user = 'user';
+  $password = 'password';
+  
+  //ログイン認証
+  if (isset($_POST['name']) && isset($_POST['password'])) {
+      $name = $_POST['name'];
+      $pass = $_POST['password'];    
+      $sql = "SELECT * FROM users WHERE name = '$name' AND password = '$pass';";
+      $dbh = new PDO($dsn, $user, $password);
+      $result = $dbh->query($sql);
+      $result = $result->fetch();
+      if ($result) {
+          $msg = 'ログインしました。<br>RiSTCTF{SQL_Injection_is_very_easy}';
+          $link = '<a href="index.php">ホーム</a>';
+      } else {
+          $msg = 'ユーザーネームもしくはパスワードが間違っています。';
+          $link = '<a href="login.php">戻る</a>';
+      }
+  } 
+  ?>
+    <form action="index.php" method="POST">
+        <h1>Login</h1>
+        <input type="text" name="name" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <input class="btn" type="submit" value="Login"/>
+        <?php if(isset($msg)){echo $msg;} ?>
+    </form>
+    </body>
+  </html>
+  `,
+}
+
 const reqBody = {
   code: '',
   path: './index.php',
@@ -120,8 +172,8 @@ const Manaco: React.FC = () => {
         options={{
           fontSize: 16,
         }}
-        // defaultLanguage={data.mode}
-        // defaultValue={data.code}
+        defaultLanguage={data.mode}
+        defaultValue={data.code}
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         onMount={handleEditorDidMount}
