917248affef92d1eb814c017023d72748f6746b8
malsuke <sukemal096@gmail.com>
2024-01-14 22:38:31 +0900

    update: モーダルで隠れてしまわないようにzIndexを修正

--- src/components/common/confetti/index.tsx
+++ src/components/common/confetti/index.tsx
@@ -4,16 +4,18 @@ import Confetti from 'canvas-confetti'
 
 export function Cheer() {
   Confetti({
-    particleCount: 30,
+    particleCount: 200,
     angle: 60,
     spread: 55,
     origin: { x: 0 },
+    zIndex: 99999,
   })
   // right
   Confetti({
-    particleCount: 30,
+    particleCount: 200,
     angle: 120,
     spread: 55,
     origin: { x: 1 },
+    zIndex: 99999,
   })
 }
