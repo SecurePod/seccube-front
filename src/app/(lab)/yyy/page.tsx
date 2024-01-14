c2dba276effdaed4acf6cda6e752d5063b6fdccd
malsuke <sukemal096@gmail.com>
2024-01-14 22:38:08 +0900

    add : Cheerのテスト

---
+++ src/app/(lab)/yyy/page.tsx
@@ -0,0 +1,10 @@
+import { Cheer } from '@/components/common/confetti'
+import { Button } from '@mui/material'
+
+export default function page() {
+  return (
+    <>
+      <Button onClick={Cheer}>here</Button>
+    </>
+  )
+}
