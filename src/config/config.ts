2786f639630fb5e1f2b904be481abdfd00112d9f
malsuke <sukemal096@gmail.com>
2024-02-29 02:07:26 +0900

    update: デフォルト値を削除

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = (process.env.NEXT_PUBLIC_API_URL as string) ?? 'api.yuki-ishida.tech'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string
 export const HOST = process.env.NEXT_PUBLIC_HOST as string
